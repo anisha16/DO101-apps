40a1bde977172c29e27ba3442dae47414c9ac247
anisha16 <anishahas1600@gmail.com>
2024-04-26 01:01:45 -0400

    updated app to v2

--- version/app.js
+++ version/app.js
@@ -3,9 +3,9 @@ app = express();
 
 var response;
 
-app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+app.get('/', function (req, res) {
+    response = 'This is version 2 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
